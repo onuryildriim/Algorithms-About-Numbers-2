ffef612ad933f65a9b878f082ce5f25f7ff9f2e1
Onur Yıldırım <onur.yildirim.2017@ogr.iu.edu.tr>
2022-02-18 19:33:43 +0300

    Create MainPage.java

---
+++ MainPage.java
@@ -0,0 +1,13 @@
+package myJava;
+
+public class MainPage {
+
+	
+	public static void main(String[] args) {
+		//Algorithm to find the sum of odd and even numbers between 1 and 100 separately.
+		Algorithm algorithm = new Algorithm();
+		algorithm.start();
+
+	}
+
+}
